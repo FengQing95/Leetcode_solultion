11a36d10e014dd79e1d34e7053090bad59691fcf
FengQing95 <54967714+FengQing95@users.noreply.github.com>
2020-06-16 09:43:47 +0800

    Change to Java

--- 21. Merge Two Sorted Lists.py
+++ 21. Merge Two Sorted Lists.py
@@ -1,25 +1,28 @@
- Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+Best solution.
 
-class Solution:
-    def mergeTwoLists(self, l1, l2):
-        if not l1:
-            return l2
-        if not l2:
-            return l1
+```java
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1);
+        ListNode curNode = head;
+
+        while(l1!=null || l2!=null) {
+            int val1 = (l1==null ? Integer.MAX_VALUE : l1.val);
+            int val2 = (l2==null ? Integer.MAX_VALUE : l2.val);
+            
+            if(val1 < val2) {
+                curNode.next = new ListNode(val1);
+                l1 = l1.next;
+            } else {
+                curNode.next = new ListNode(val2);
+                l2 = l2.next;
+            }
+            
+            curNode = curNode.next;
+        }
         
-        cur1 = l1; cur2 = l2
-        res = ListNode(-1); cur = res
-        while cur1 or cur2:
-            if not cur1 or (cur2 and cur1.val > cur2.val):
-                cur.next = ListNode(cur2.val)
-                cur2 = cur2.next
-            else:
-                cur.next = ListNode(cur1.val)
-                cur1 = cur1.next
-            cur = cur.next     
-        
-        return res.next
+        return head.next;
+    }
+}
+```
+
